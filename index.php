27380814726345c8b6ebd0e15f6ea9bb599af5a2
Sergey <isa86box@gmail.com>
2021-08-08 17:37:35 +0300

    немного переписал обработу ошибок

--- index.php
+++ index.php
@@ -8,32 +8,31 @@ mysqli_set_charset($link, "utf8");
 
 if (!$link) {
     $error = mysqli_connect_error();
-    $layout_content = include_template('error.php', ['error' => $error]);
+    return print(include_template('error.php', ['error' => $error]));
+}
+
+$sql = 'SELECT * FROM categories';
+$result = mysqli_query($link, $sql);
+
+if ($result) {
+    $categoriesList = mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 else {
-    $sql = 'SELECT * FROM categories';
-    $result = mysqli_query($link, $sql);
-
-    if ($result) {
-        $categoriesList = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    }
-    else {
-        $error = mysqli_error($link);
-        $content = include_template('error.php', ['error' => $error]);
-    }
-
-    $page_content = include_template('main.php', [
-        'goodsList' => $goodsList,
-        'categoriesList' => $categoriesList
-    ]);
-
-    $layout_content = include_template('layout.php', [
-        'isAuth' => $isAuth,
-        'userName' => $userName,
-        'content' => $page_content,
-        'categoriesList' => $categoriesList,
-        'title' => 'GifTube - Главная страница'
-    ]);
+    $error = mysqli_error($link);
+    return print(include_template('error.php', ['error' => $error]));
 }
+
+$page_content = include_template('main.php', [
+    'goodsList' => $goodsList,
+    'categoriesList' => $categoriesList
+]);
+
+$layout_content = include_template('layout.php', [
+    'isAuth' => $isAuth,
+    'userName' => $userName,
+    'content' => $page_content,
+    'categoriesList' => $categoriesList,
+    'title' => 'GifTube - Главная страница'
+]);
 
 print($layout_content);
