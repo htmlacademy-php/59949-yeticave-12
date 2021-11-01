5020464dc9afe8048c884345c547e8d8fd405774
Sergey <isa86box@gmail.com>
2021-11-01 22:35:09 +0300

    подстановка в форму переданных значений для предотвращения сброса полей

--- templates/add-lot.php
+++ templates/add-lot.php
@@ -12,15 +12,24 @@
     <div class="form__container-two">
         <div class="form__item <?= isset($errors['lot-name']) ? 'form__item--invalid' : ''; ?>">
             <label for="lot-name">Наименование <sup>*</sup></label>
-            <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота">
+            <input
+                id="lot-name"
+                type="text"
+                name="lot-name"
+                placeholder="Введите наименование лота"
+                value="<?= $_POST['lot-name'] ?? "" ?>"
+            >
             <span class="form__error"><?= $errors['lot-name'] ?></span>
         </div>
         <div class="form__item <?= isset($errors['category']) ? 'form__item--invalid' : ''; ?>">
             <label for="category">Категория <sup>*</sup></label>
             <select id="category" name="category">
-                <option value="" selected="selected" hidden="hidden">Выберите категорию</option>
+                <option value="" selected hidden="hidden">Выберите категорию</option>
                 <?php foreach ($categoriesList as $category) : ?>
-                    <option value="<?= htmlspecialchars($category['code']); ?>">
+                    <option
+                        value="<?= htmlspecialchars($category['code']); ?>"
+                        <?php if($_POST['category'] === $category['code']){echo ' selected';} ?>
+                    >
                         <?= htmlspecialchars($category['title']); ?>
                     </option>
                 <?php endforeach; ?>
@@ -30,7 +39,7 @@
     </div>
     <div class="form__item form__item--wide <?= isset($errors['message']) ? 'form__item--invalid' : ''; ?>">
         <label for="message">Описание <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите описание лота"></textarea>
+        <textarea id="message" name="message" placeholder="Напишите описание лота"><?= $_POST['message'] ?? ""; ?></textarea>
         <span class="form__error"><?= $errors['message'] ?></span>
     </div>
     <div class="form__item form__item--file">
@@ -45,17 +54,24 @@
     <div class="form__container-three">
         <div class="form__item form__item--small <?= isset($errors['lot-rate']) ? 'form__item--invalid' : ''; ?>">
             <label for="lot-rate">Начальная цена <sup>*</sup></label>
-            <input id="lot-rate" type="text" name="lot-rate" placeholder="0">
+            <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="<?= $_POST['lot-rate'] ?? "" ?>">
             <span class="form__error"><?= $errors['lot-rate'] ?></span>
         </div>
         <div class="form__item form__item--small <?= isset($errors['lot-step']) ? 'form__item--invalid' : ''; ?>">
             <label for="lot-step">Шаг ставки <sup>*</sup></label>
-            <input id="lot-step" type="text" name="lot-step" placeholder="0">
+            <input id="lot-step" type="text" name="lot-step" placeholder="0" value="<?= $_POST['lot-step'] ?? "" ?>">
             <span class="form__error"><?= $errors['lot-step'] ?></span>
         </div>
         <div class="form__item <?= isset($errors['lot-date']) ? 'form__item--invalid' : ''; ?>">
             <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
-            <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
+            <input
+                class="form__input-date"
+                id="lot-date"
+                type="text"
+                name="lot-date"
+                placeholder="Введите дату в формате ГГГГ-ММ-ДД"
+                data-default-date='<?= $_POST['lot-date'] ?? "" ?>'
+            >
             <span class="form__error"><?= $errors['lot-date'] ?></span>
         </div>
     </div>
