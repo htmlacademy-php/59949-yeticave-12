7139ed0c759eebf532db94bf39dd14b53e40f3c5
Sergey <isa86box@gmail.com>
2021-11-20 01:19:50 +0300

    получение лота по его id: строго указал в запросе необходимые поля

--- queries/lot-by-id.php
+++ queries/lot-by-id.php
@@ -3,7 +3,8 @@
 require_once ('db-methods.php');
 
 function fetch_lot_by_id($conn, $id) {
-    $sql = "SELECT l.*, c.title AS category_title, (initial_price + IFNULL(SUM(b.amount), 0)) AS current_price FROM lots l
+    $sql = "SELECT l.id, l.title, c.title AS category_title, img_path, description, expiry_dt, bet_step,
+       (initial_price + IFNULL(SUM(b.amount), 0)) AS current_price FROM lots l
     JOIN categories c ON l.category_id = c.id
     LEFT JOIN bets b ON l.id = b.lot_id
     WHERE l.id = $id
