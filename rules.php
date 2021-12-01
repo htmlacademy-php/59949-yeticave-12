bc450b3a873c9eade9d97780d21736d653ed4490
Sergey <isa86box@gmail.com>
2021-12-02 00:00:30 +0300

    структура правил для валидации файлов

--- rules.php
+++ rules.php
@@ -7,7 +7,7 @@ $lot_descr_min_len = 10;
 $lot_descr_max_len = 1000;
 
 
-$validation_rules = [
+$lot_create_data_validation_rules = [
     [
         'field_name' => 'lot-name',
         'validations' => [
@@ -48,6 +48,17 @@ $validation_rules = [
         'validations' => [
             ['method' => 'isNotEmpty', 'error_msg' => 'Напишите описание лота'],
             ['method' => 'isCorrectLength', 'param1' => $lot_descr_min_len, 'param2' => $lot_descr_max_len, 'error_msg' => "Значение должно быть от $lot_descr_min_len до $lot_descr_max_len символов"]
+        ]
+    ]
+];
+
+$lot_create_files_validation_rules = [
+    [
+        'file_name' => 'lot-img',
+        'validations' => [
+            ['method' => 'isFileSelected', 'error_msg' => 'Добавьте изображение лота'],
+            ['method' => 'isFileTypeCorrect', 'param1' => 'image/jpeg', 'param2' => 'image/png', 'error_msg' => 'Изображение в формате jpeg/png'],
+            ['method' => 'isFileSizeCorrect', 'param1' => 2000000, 'error_msg' => 'Максимальный размер файла: 2Мб']
         ]
     ]
 ];
