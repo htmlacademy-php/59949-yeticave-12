0c0becc8f11112ded48c49f957c9b9eef485afb2
Sergey <isa86box@gmail.com>
2021-12-08 21:15:18 +0300

    валидация формы аутентификации

--- login.php
+++ login.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once('db-methods.php');
+require_once('validations.php');
 require_once('queries/categories.php');
+require_once('rules.php');
 require_once('helpers.php');
 
 $db_conn = get_db_connect();
@@ -21,5 +23,12 @@ if (!$categories_list) {
 }
 
 $errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    global $login_validation_rules;
+    $filteredData = filterDataByRules($_POST, $login_validation_rules);
+
+    $errors = validateForm($filteredData, $login_validation_rules);
+}
 
 show_screen('login.php', 'Аутентификация', 'errors', $errors, $categories_list);
