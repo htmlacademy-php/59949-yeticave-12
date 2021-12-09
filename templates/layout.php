6b25a2df52bae1c57a3eaf000549e44d436f99ec
Sergey <isa86box@gmail.com>
2021-12-09 18:28:05 +0300

    добавил сценарий разлогинивания пользователя и очистки сессии

--- templates/layout.php
+++ templates/layout.php
@@ -26,7 +26,7 @@
                     <div class="user-menu__logged">
                         <p><?= htmlspecialchars($user['name']); ?></p>
                         <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-                        <a class="user-menu__logout" href="#">Выход</a>
+                        <a class="user-menu__logout" href="logout.php">Выход</a>
                     </div>
                 <?php else : ?>
                     <ul class="user-menu__list">
