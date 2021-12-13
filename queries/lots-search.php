beb4faad0a1ff348e63d18f3ce5a00ad7b8193af
Sergey <isa86box@gmail.com>
2021-12-13 23:21:24 +0300

    создал отдельный запрос на поиск по лотам

---
+++ queries/lots-search.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @param mysqli $conn
+ * @param string $search
+ * @return array|false
+ */
+function search_lots(mysqli $conn, string $search) {
+    $sql = "SELECT l.id, l.title, description, c.title category_title, expiry_dt, initial_price, img_path
+    FROM lots l
+    LEFT JOIN bets b ON l.id = b.lot_id
+    JOIN categories c ON l.category_id = c.id
+    WHERE MATCH(l.title, description) AGAINST('$search')
+    GROUP BY l.id
+    ORDER BY l.created_at DESC";
+
+    return fetch_from_db($conn, $sql);
+}
