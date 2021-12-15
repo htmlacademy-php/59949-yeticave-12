afd47d63d24d44c9c4db4799ece5612644f6571a
Sergey <isa86box@gmail.com>
2021-12-15 12:03:56 +0300

    динамический шаблон для пагинации

--- templates/main.php
+++ templates/main.php
@@ -50,13 +50,28 @@
                 <?php endforeach; ?>
             <?php endif; ?>
         </ul>
-        <ul class="pagination-list">
-            <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
-            <li class="pagination-item pagination-item-active"><a>1</a></li>
-            <li class="pagination-item"><a href="#">2</a></li>
-            <li class="pagination-item"><a href="#">3</a></li>
-            <li class="pagination-item"><a href="#">4</a></li>
-            <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
-        </ul>
+        <?php if (count($pages) > 1): ?>
+            <ul class="pagination-list">
+                <li class="pagination-item pagination-item-prev">
+                    <?php if ($cur_page <= 1): ?>
+                        <a>Назад</a>
+                    <?php else : ?>
+                        <a href="/?page=<?= $cur_page - 1; ?>">Назад</a>
+                    <?php endif; ?>
+                </li>
+                <?php foreach ($pages as $page): ?>
+                    <li class="pagination-item <?= ($page == $cur_page) ? 'pagination-item-active' : ''; ?>">
+                        <a href="/?page=<?= $page; ?>"><?= $page; ?></a>
+                    </li>
+                <?php endforeach; ?>
+                <li class="pagination-item pagination-item-next">
+                    <?php if ($cur_page >= count($pages)): ?>
+                        <a>Вперед</a>
+                    <?php else : ?>
+                        <a href="/?page=<?= $cur_page + 1; ?>">Вперед</a>
+                    <?php endif; ?>
+                </li>
+            </ul>
+        <?php endif; ?>
     </section>
 </main>
