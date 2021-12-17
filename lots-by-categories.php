1755b06d07065560035f13118c7fa9dd5c5ff5ca
Sergey <isa86box@gmail.com>
2021-12-17 15:25:23 +0300

    запрос лотов по id категорий в сценарии

--- lots-by-categories.php
+++ lots-by-categories.php
@@ -1,7 +1,7 @@
 <?php
 
 $db_conn = require_once('init.php');
-require_once('queries/lots.php');
+require_once('queries/lots-by-category.php');
 
 $categories_list = get_categories($db_conn);
 
@@ -12,6 +12,12 @@ if (!$categories_list) {
 }
 
 $lots_list = [];
+
+$category_id = $_GET['category'] ?? '';
+
+if ($category_id) {
+    $lots_list = get_lots_by_category($db_conn, $category_id);
+}
 
 $lot_cards_list_tmpl = get_lot_cards_list_template($lots_list);
 $categories_list_tmpl = get_categories_list_template($categories_list);
