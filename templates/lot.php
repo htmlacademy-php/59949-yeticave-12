2a219a03022e2506dbc1340ae551f97fe9300415
Sergey <isa86box@gmail.com>
2021-12-29 23:46:05 +0300

    статичный шаблон формы добавления ставки на лот

--- templates/lot.php
+++ templates/lot.php
@@ -27,6 +27,14 @@
                             Мин. ставка <span><?= formatPrice($lot['bet_step']); ?></span>
                         </div>
                     </div>
+                    <form class="lot-item__form" action="lot.php" method="post" autocomplete="off">
+                        <p class="lot-item__form-item form__item form__item--invalid">
+                            <label for="cost">Ваша ставка</label>
+                            <input id="cost" type="text" name="cost" placeholder="12 000">
+                            <span class="form__error">Введите наименование лота</span>
+                        </p>
+                        <button type="submit" class="button">Сделать ставку</button>
+                    </form>
                 </div>
             </div>
         </div>
