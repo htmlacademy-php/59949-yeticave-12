32891558b5967fa42706585caa955805a7a3ca5d
Sergey <isa86box@gmail.com>
2021-12-29 20:57:02 +0300

    запрос на получение категории по её id

---
+++ queries/category-by-id.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @param mysqli $conn
+ * @param string $category_id
+ * @return array|false
+ */
+function get_category_by_id(mysqli $conn, string $category_id) {
+    $sql = "SELECT id, title FROM categories c WHERE c.id = ?";
+
+    return fetch_from_db_by_params($conn, $sql, [$category_id])[0];
+}
