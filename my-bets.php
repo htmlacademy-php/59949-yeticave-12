4c3f59e78ad05bf09f5c2ff30c51b02db0ece7bc
Sergey <isa86box@gmail.com>
2022-01-14 18:48:11 +0300

    создал шаблон и сценарий для страницы "мои ставки" (пока со статикой)

---
+++ my-bets.php
@@ -0,0 +1,21 @@
+<?php
+
+$db_conn = require_once('init.php');
+
+$categories_list = get_categories($db_conn);
+
+if (!$categories_list) {
+    $error = get_db_error($db_conn);
+    show_error($error);
+    exit();
+}
+
+$categories_list_tmpl = get_categories_list_template($categories_list);
+
+$display_params = [
+    'file' => 'my-bets.php',
+    'title' => 'Мои ставки',
+    'categories_list_tmpl' => $categories_list_tmpl
+];
+
+show_screen($display_params);
