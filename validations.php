63d717c25e239d07bc4cd5b783e2401f931b9274
Sergey <isa86box@gmail.com>
2022-04-12 23:02:31 +0300

    валидация: проверка на наличие ключей

--- validations.php
+++ validations.php
@@ -59,7 +59,7 @@ function isFileSelected(?array $file): bool
  */
 function isFileTypeCorrect(array $file, string $type1, string $type2): bool
 {
-    $fileType = mime_content_type($file['tmp_name']);
+    $fileType = mime_content_type($file['tmp_name'] ?? '');
 
     if ($fileType !== $type1 && $fileType !== $type2) {
         return false;
@@ -164,14 +164,18 @@ function validateForm(array $data, array $rules): array
     $errors = [];
 
     foreach ($rules as $rule) {
-        $key = $rule['field_name'];
-        $data_value = $data[$key];
+        $key = $rule['field_name'] ?? '';
+        $data_value = $data[$key] ?? '';
 
-        foreach ($rule['validations'] as $validation) {
+        foreach ($rule['validations'] ?? [] as $validation) {
+            if (empty($validation['method']) || !function_exists($validation['method'])) {
+                break;
+            }
+
             $is_valid = $validation['method']($data_value, $validation['param1'] ?? '', $validation['param2'] ?? '');
 
             if (!$is_valid) {
-                $errors[$key] = $validation['error_msg'];
+                $errors[$key] = $validation['error_msg'] ?? '';
                 break;
             }
         }
